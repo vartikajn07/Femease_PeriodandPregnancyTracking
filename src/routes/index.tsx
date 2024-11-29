66cf13b98a219811fb0900e2e4f73a531b482940
vartikajn07 <vartikajn1107@gmail.com>
2024-11-29 16:02:56 +0530

    pregnancy onboarding screens

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,7 @@ import SplashScreen from '../screens/Splash screen/Splashscreen';
 import PeriodTrackingOnboarding from '../screens/Period tracking/Onboarding/index';
 import PeriodTrackingHome from '../screens/Period tracking/Home';
 import PregnancyOnboarding from '../screens/Pregnancy tracking/Onboarding';
+import PregnancyHome from '../screens/Pregnancy tracking/Home';
 
 const Stack = createStackNavigator();
 
@@ -30,6 +31,11 @@ const AppNavigator = () => (
       <Stack.Screen
         name="PregnancyOnboarding"
         component={PregnancyOnboarding}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="PregnancyHome"
+        component={PregnancyHome}
         options={{headerShown: false}}
       />
     </Stack.Navigator>
