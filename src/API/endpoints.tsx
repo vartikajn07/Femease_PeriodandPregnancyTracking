ba924946a4a939cd3c09b9428eecde60925d1e26
Vartika Jain <vartikajn1107@gmail.com>
2024-12-28 17:14:14 +0530

    api endpoints and base url

---
+++ src/API/endpoints.tsx
@@ -0,0 +1,14 @@
+
+const BASE_URL = 'https://period-pregnancy-tracker-app.onrender.com/'
+
+const endpoints = {
+    // Auth endpoints
+    LOGIN: `${BASE_URL}api/auth/login`,
+    REGISTER: `${BASE_URL}api/auth/register`
+    
+   
+  
+    // Add more endpoints here
+  };
+  
+  export default endpoints;
